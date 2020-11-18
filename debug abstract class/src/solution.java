abae225496f80cd898a30adfb06da7fc3ef4cfd8
8700nd <rossilyx@gmail.com>
2020-11-17 16:05:54 -0800

    TinNews

---
+++ debug abstract class/src/solution.java
@@ -0,0 +1,24 @@
+public class solution {
+    abstract class demo{
+        public int a;
+        demo(){
+            a = 10;
+        }
+        abstract public void set();
+        abstract public void get();
+        
+    }
+    class Test extends demo{
+        public void set(int a){
+            this.a = a;
+        }
+        final public void get(){
+            System.out.println("a = " + a);
+        }
+        public static void main(String[] args){
+            Test obj = new Test();
+            obj.set(20);
+            obj.get();
+        }
+    }
+}
